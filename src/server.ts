1701173a9e2a04db1d9e72e6032848106890c440
vinibalbino <silvavinicius2018@gmail.com>
2021-05-07 13:34:54 -0400

    Adding helmet package for security

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-
+import helmet from 'helmet'
 import routes from './routes';
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(routes);
